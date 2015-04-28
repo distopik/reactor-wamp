fb4a0c837abf3dcc886acb1b6a4dd469a86255ee
Bojan Šernek <bojan@distopiksound.com>
2015-04-28 20:46:04 +0200

    added jetty websocket and javax.servlet as deps continuing initial work on accepting client connections

--- src/test/java/com/distopik/wamp/test/ReactorBasics.java
+++ src/test/java/com/distopik/wamp/test/ReactorBasics.java
@@ -1,13 +1,25 @@
 package com.distopik.wamp.test;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.eclipse.jetty.websocket.WebSocket;
+import org.eclipse.jetty.websocket.WebSocketServlet;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import reactor.Environment;
+import reactor.bus.Event;
+import reactor.bus.EventBus;
+import reactor.bus.selector.Selector;
+import reactor.bus.selector.Selectors;
 
 
 public class ReactorBasics {
+	private static class Trade {
+		
+	}
+	
 	private Environment env;
 	
 	@Before
@@ -23,5 +35,20 @@ public class ReactorBasics {
 	
 	@Test
 	public void simple() {
+		final EventBus serverReactor = EventBus.create(env);
+		final Selector<String> tradeExecute = Selectors.object("trade.execute");
+		serverReactor.on(tradeExecute, (Event<Trade> event) -> {
+			System.out.println("event!");
+		});
+		
+		WebSocketServlet wss = new WebSocketServlet() {
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public WebSocket doWebSocketConnect(HttpServletRequest request, String protocol) {
+				System.out.println("CONNECTED: protocol: " + protocol);
+				return null;
+			}
+		};
 	}
 }
